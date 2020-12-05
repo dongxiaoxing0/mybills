81eda43bef5559cd0ea36e553c372b4c2c7dfc12
dongxiaoxing <dongxiaoxing0@gmail.com>
2020-12-05 22:23:53 +0800

    deploy

---
+++ precache-manifest.49a83623ec74e24c3b4a075e7686e33d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2588478e08948dc3ca4",
+    "url": "/mybills/css/app.d26089ab.css"
+  },
+  {
+    "revision": "b44cc438d0759ba745712965e84c734e",
+    "url": "/mybills/index.html"
+  },
+  {
+    "revision": "c2588478e08948dc3ca4",
+    "url": "/mybills/js/app.52bb067d.js"
+  },
+  {
+    "revision": "2db31771c1720c13c2b1",
+    "url": "/mybills/js/chunk-vendors.2a63b496.js"
+  },
+  {
+    "revision": "23fc8e2b2be3b08ea52a8052a3e2d6ff",
+    "url": "/mybills/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mybills/robots.txt"
+  }
+]);
